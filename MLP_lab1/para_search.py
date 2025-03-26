3d7373114270688b98470371c4e7a493cde3d4ce
Daisy-Butter <22307140084@m.fudan.edu.cn>
2025-03-26 16:55:50 +0800

    changes to train.py

--- MLP_lab1/para_search.py
+++ MLP_lab1/para_search.py
@@ -1,1 +1,11 @@
 # 参数查找环节要求调节学习率、隐藏层大小、正则化强度等超参数，观察并记录模型在不同超参数下的性能
+
+from train import Trainer
+from MLP_model import MLP
+
+lr = 0.01
+hidden_sizes = [64, 32]
+reg_strength = 1e-3
+epochs = 20
+batch_size = 128
+print_every = 5
