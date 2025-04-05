bbb0c0160903cb189361a35790d5338619353978
Daisy-Butter <22307140084@m.fudan.edu.cn>
2025-04-05 21:26:40 +0800

    change hyperparameters(epoch=12 to prevent overfitting)

--- LAB1/config.py
+++ LAB1/config.py
@@ -1,10 +1,10 @@
 class config:
     INPUT_DIM = 3072
-    hidden_dim = 512
+    hidden_dim = 512 # we use same size for both hidden layers
     output_dim = 10
-    LEARNING_RATE = 0.01
+    LEARNING_RATE = 0.001
     decay_rate = 0.95
     beta = 0.001
-    batch_size = 128
-    EPOCHS = 50
+    batch_size = 256
+    EPOCHS = 12
     activation = 'leaky_relu'
